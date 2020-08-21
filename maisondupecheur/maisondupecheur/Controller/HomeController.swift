64aa62e39d00031744070cfc2dfa0a7f2de8f93e
Jérôme Guèrin <guerinjerome@outlook.fr>
2020-08-21 15:31:53 +0200

    build(view+controller+model): home logic finished

--- maisondupecheur/maisondupecheur/Controller/HomeController.swift
+++ maisondupecheur/maisondupecheur/Controller/HomeController.swift
@@ -9,15 +9,12 @@
 import UIKit
 import MapKit
 
-enum InfosChoice {
-    case infos, weather, pictures, phone, mail, adress
-}
-
 final class HomeController: UIViewController {
 
     // MARK: - Properties
     
-    private var infosChoice: InfosChoice!
+    private let house = House()
+    private var imageIndex = 0
     
     // MARK: - Outlets
 
@@ -35,6 +32,7 @@ final class HomeController: UIViewController {
     @IBOutlet private weak var houseImages: UIImageView!
     @IBOutlet private weak var buttonArrowLeft: UIButton!
     @IBOutlet private weak var buttonArrowRight: UIButton!
+    @IBOutlet private weak var stackButton: UIButton!
     
     // MARK: - Initializer
 
@@ -48,14 +46,34 @@ final class HomeController: UIViewController {
     
     @IBAction func tappOnButtons(_ sender: UIButton) {
         animateToHideOnLeft()
+        tapOnButton(sender.tag)
     }
     
     @IBAction func tapLocationAround() {
         animateToHideLittleButtons()
     }
     
+    @IBAction func tapActionButton() {
+        if let url = URL(string: "tel://\(house.phoneNumber)") {
+            UIApplication.shared.open(url)
+        }
+    }
+    
     @IBAction func tapDismissButton() {
         showAllButtons()
+    }
+    
+    @IBAction func tapArrowButtons(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            imageIndex -= 1
+            setUpImageStack()
+        case 1:
+            imageIndex += 1
+            setUpImageStack()
+        default:
+            return
+        }
     }
     
     // MARK: - Private functions
@@ -70,8 +88,8 @@ final class HomeController: UIViewController {
         houseImage.layer.borderColor = UIColor.white.cgColor
         
         let basicLocation = CLLocationCoordinate2D(latitude: 45.72175979614258, longitude: -1.1450577974319458)
-        let regionLatitudeMeters: CLLocationDistance = 1000
-        let regionLongitudeMeters: CLLocationDistance = 1000
+        let regionLatitudeMeters: CLLocationDistance = 15000
+        let regionLongitudeMeters: CLLocationDistance = 15000
         let region = MKCoordinateRegion(center: basicLocation, latitudinalMeters: regionLatitudeMeters, longitudinalMeters: regionLongitudeMeters)
         houseMap.mapType = .satellite
         houseMap.setRegion(region, animated: true)
@@ -85,6 +103,9 @@ final class HomeController: UIViewController {
         backgroundInfoView.layer.cornerRadius = 20
         backgroundInfoView.layer.borderWidth = 3
         backgroundInfoView.layer.borderColor = UIColor.white.cgColor
+        stackButton.layer.cornerRadius = 10
+        stackButton.layer.borderWidth = 2
+        stackButton.layer.borderColor = UIColor.white.cgColor
         
         indexPage.isUserInteractionEnabled = false
     }
@@ -142,6 +163,76 @@ final class HomeController: UIViewController {
                 })
             }
         }
+    }
+    
+    private func tapOnButton(_ number: Int) {
+        switch number {
+        case 2:
+            hideAllImageSystem(false)
+            setUpImageStack()
+        case 1, 3, 4, 5:
+            hideAllImageSystem(true)
+            setUpLabels(number)
+        default:
+            return()
+        }
+    }
+    
+    private func hideAllImageSystem(_ bool: Bool) {
+        indexPage.isHidden = bool
+        houseImages.isHidden = bool
+        buttonArrowLeft.isHidden = bool
+        buttonArrowRight.isHidden = bool
+        bodyLabel.isHidden = !bool
+    }
+    
+    private func setUpLabels(_ number: Int) {
+        bodyLabel.font = UIFont(name: "Verdana", size: 20)
+        bodyLabel.textAlignment = .center
+        stackButton.isHidden = true
+        switch number {
+        case 1:
+            titleLabel.text = "INFOS"
+            bodyLabel.text = house.infos
+            bodyLabel.font = UIFont(name: "Verdana", size: 12)
+            bodyLabel.textAlignment = .justified
+        case 3:
+            titleLabel.text = "TÉLÉPHONE"
+            bodyLabel.text = house.phoneNumber
+            stackButton.isHidden = false
+            stackButton.setTitle("APPELER", for: .normal)
+        case 4:
+            titleLabel.text = "MAIL"
+            bodyLabel.text = house.mail
+        case 5:
+            titleLabel.text = "ADRESSE"
+            bodyLabel.text = house.adress
+        default:
+            return
+        }
+    }
+    
+    private func setUpImageStack() {
+        stackButton.isHidden = true
+        titleLabel.text = "IMAGES"
+        indexPage.currentPage = imageIndex
+        if imageIndex == 0 {
+            buttonArrowLeft.layer.opacity = 0
+            buttonArrowLeft.isUserInteractionEnabled = false
+        } else {
+            buttonArrowLeft.layer.opacity = 1
+            buttonArrowLeft.isUserInteractionEnabled = true
+        }
+        if imageIndex == house.pictures.count-1 {
+            buttonArrowRight.layer.opacity = 0
+            buttonArrowRight.isUserInteractionEnabled = false
+        } else {
+            buttonArrowRight.layer.opacity = 1
+            buttonArrowRight.isUserInteractionEnabled = true
+        }
+        houseImages.image = UIImage(named: house.pictures[imageIndex])
+        houseImages.layer.borderWidth = 3
+        houseImages.layer.borderColor = UIColor.white.cgColor
     }
 }
 
---
+++ maisondupecheur/maisondupecheur/Model/House.swift
@@ -0,0 +1,30 @@
+//
+//  House.swift
+//  maisondupecheur
+//
+//  Created by Jérôme Guèrin on 21/08/2020.
+//  Copyright © 2020 Jérôme Guèrin. All rights reserved.
+//
+
+import Foundation
+
+final class House {
+    
+    // MARK: - Properties
+    
+    let phoneNumber = "0662014871"
+    let mail = "jose.guerin@orange.fr"
+    let adress = """
+    31 rue Léon Nicolle
+    17570, Les Mathes
+    FRANCE
+    """
+    let pictures = ["maison", "terrasse", "cuisine", "salon", "lit", "douche", "chambre"]
+    let infos = """
+    60 m², 5 personnes, 260-570€/semaine, 60-100€/nuit
+
+    Située dans le centre du village des Mathes, au coeur de la Charente-Maritime, à 20 min de Royan et 1h de La Rochelle sur la route des plages, à proximité de tous les commerces (boulangerie, boucherie, supermarché, poste, marché, église, ...), à 5 km de l'océan.
+    Possibilité d'accés aux plages par un étendu réseau de pistes cyclables qui longent toutes les plages.
+    Voir l'adresse dans l'onglet "Enveloppe".
+    """
+}
